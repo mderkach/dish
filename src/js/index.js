e6851111bd3989377742c0c6e1c0b74f8df08745
mderkach <derkach94@gmail.com>
2020-03-31 12:25:52 +0300

    screen order

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,8 @@
 // Main js file
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#import-js-files
+import Inputmask from "inputmask";
+let phones = document.querySelectorAll('input[name="phone"]');
+let inputmask = new Inputmask("+7(999)999-99-99");
+phones.forEach(input => inputmask.mask(input));
+
 import "./slider__banner";
